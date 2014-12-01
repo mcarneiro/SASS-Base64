a59336aeb05dd7538c6b14d9a5d9c2789a4d8857
Marcelo Miranda Carneiro <mcarneiro@fbiz.com.br>
2014-12-01 14:57:01 -0200

    new standalone function str64 for just converting a file to base64, without considering the mime-type;

--- url64.rb
+++ url64.rb
@@ -1,36 +1,44 @@
 require "base64"
 require "sass"
-require "smusher"
 
 module Sass::Script::Functions
-    def url64(image)
-        assert_type image, :String
+    def convertToB64(file_path)
+        assert_type file_path, :String
 
         # compute file/path/extension
         base_path = "../../.."
         root = File.expand_path(base_path, __FILE__)
-        path = image.to_s[4,image.to_s.length-5]
+        path = file_path.to_s[4,file_path.to_s.length-5]
         fullpath = File.expand_path(path, root)
-        absname = File.expand_path(fullpath)
-        ext = File.extname(path)
-
-        # optimize image
-        if ext == ".gif" || ext == ".jpg" || ext == ".png"
-            Smusher::optimize_image(absname)
-        elsif ext == ".svg"
-            # `python scour/scour.py -i ` + absname + ` -o ` + absname
-            ext = ext + "+xml"
-        end
 
         # base64 encode the file
         file = File.open(fullpath, "rb")
         text = file.read
         file.close
         text_b64 = Base64.encode64(text).gsub(/\r/,"").gsub(/\n/,"")
-        contents = "url(data:image/" + ext[1,ext.length-1] + ";base64," + text_b64 + ")"
 
-        Sass::Script::String.new(contents)
+        return text_b64
 
     end
+    def str64(file_path)
+        assert_type file_path, :String
+        Sass::Script::String.new(convertToB64(file_path))
+    end
+    def url64(file_path)
+        assert_type file_path, :String
+
+        # compute file/path/extension
+        path = file_path.to_s[4,file_path.to_s.length-5]
+        ext = File.extname(path)
+
+        if ext == ".svg"
+            ext = ext + "+xml"
+        end
+
+        contents = "url(data:image/" + ext[1,ext.length-1] + ";base64," + convertToB64(file_path) + ")"
+
+        Sass::Script::String.new(contents)
+    end
     declare :url64, :args => [:string]
+    declare :str64, :args => [:string]
 end
